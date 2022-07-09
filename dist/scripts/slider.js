96d6d9a2a1f33dd22a6e4f4161e4f2374fb0bf8a
vlasmn <foxsoqa@icloud.com>
2022-07-09 14:46:54 +0400

    fix

--- dist/scripts/slider.js
+++ dist/scripts/slider.js
@@ -1,17 +1,15 @@
 // v1: Pavel Selivanov
 // v2: Vlad Garbovsky, https://garbovsky.com/
 
-$(window).bind('resize', function(e) {
-    $(".slider").on("mousemove touchmove", function (e) {
-     const slider = $(this),
-      images = $(this).find(".slider__images"),
-      imageCount = images.find(".slider__slide").length,
-      sliderCut = $(this).width() / imageCount;
+$(".slider").on("mousemove touchmove", function (e) {
+    const slider = $(this),
+    images = $(this).find(".slider__images"),
+    imageCount = images.find(".slider__slide").length,
+    sliderCut = $(this).width() / imageCount;
 
-     var offset = (e.originalEvent.touches) ? (e.originalEvent.touches[0].pageX - slider.offset().left) : offset = e.offsetX;
-      currentImg = Math.ceil(offset / sliderCut) > 0 ? Math.ceil(offset / sliderCut) : 1;
-      position = currentImg < imageCount ? currentImg : imageCount - 1;
+    var offset = (e.originalEvent.touches) ? (e.originalEvent.touches[0].pageX - slider.offset().left) : offset = e.offsetX;
+    currentImg = Math.ceil(offset / sliderCut) > 0 ? Math.ceil(offset / sliderCut) : 1;
+    position = currentImg < imageCount ? currentImg : imageCount - 1;
 
-     images.css("left", -(position * slider.width()));
-    });
-   });
+    images.css("left", -(position * slider.width()));
+});
