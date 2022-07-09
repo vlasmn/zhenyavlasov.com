832af22c49618aeb2091faf558665c59b6264f54
vlasmn <foxsoqa@icloud.com>
2022-07-09 14:51:50 +0400

    fix

--- dist/scripts/slider.js
+++ dist/scripts/slider.js
@@ -8,7 +8,7 @@ $(".slider").on("mousemove touchmove", function (e) {
     sliderCut = $(this).width() / imageCount;
 
     var offset = (e.originalEvent.touches) ? (e.originalEvent.touches[0].pageX - slider.offset().left) : offset = e.offsetX;
-    currentImg = Math.ceil(offset / sliderCut) > 0 ? Math.ceil(offset / sliderCut) : 1;
+    currentImg = Math.ceil(offset / sliderCut) >= 0 ? Math.ceil(offset / sliderCut) : 1;
     position = currentImg < imageCount ? currentImg : imageCount - 1;
 
     images.css("left", -(position * slider.width()));
